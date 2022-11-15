8263e08a9ba59556f0151d780ecf74c8589842ee
marbokEmptomies <markosoramaki@gmail.com>
2022-11-15 16:35:29 +0200

    vastausvaihtoehdoille perusqueryt

---
+++ server/routes/vastausvaihtoehdot.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../database')
+
+router.get('/', async (req, res) => {
+    try {
+        const result = 'SELECT * FROM vastausvaihtoehto ORDER BY id ASC'
+        const { rows } = await db.query(result)
+        res.status(200).send(rows)
+    } catch (error) {
+        console.log("Vastausvaihtoehtojen hakuongelma: ", error)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const { kysymys_id, nimi, onkooikein } = req.body
+        const text = 'INSERT INTO kysymys (kysymys_id, nimi, onkooikein) VALUES ($1, $2, $3) RETURNING id'
+        const result = await db.query(text, [kysymys_id, nimi, onkooikein])
+        res.status(200).send(`Uusi vastausvaihtoehto lisätty kysymykseen: ${kysymys_id}. Vastausvaihtoehdon id on ${result.rows[0].id}`)
+    } catch (error) {
+        res.status(500).send(error)
+        console.log("Kysymyksen lisäys epäonnistui", error.stack)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { uusiKysymys_id, uusiNimi, uusiOikein } = req.body
+        const text = 'UPDATE vastausvaihtoehto SET kysymys_id = ($1), nimi = ($2), onkooikein = ($3) WHERE id = ($4)'
+        console.log(req.body)
+        await db.query(text, [uusiKysymys_id, uusiNimi, uusiOikein, req.params.id])
+        res.status(201).send(`Vastausvaihtoehto muutettu ID:llä ${req.params.id}. Kysymyksen ID: ${uusiKysymys_id}`)   
+    } catch (error) {
+        res.status(500).send(error)
+        console.log("Vastausvaihtoehdon muokkaamisessa virhe ", error.stack)    
+    }
+})
+
+router.delete('/:id'), async (req, res) => {
+    try {
+        const text = 'DELETE FROM vastausvaihtoehto WHERE id=($1)'
+        const values = [req.params.id]
+        await db.query(text, values)
+        res.status(204).send(`Vastausvaihtoehto ID:llä ${req.params.id} poistettu`)
+    } catch (error) {
+        res.status(500).send(error)
+        console.log("Vastausvaihtoehdon poistaminen epäonnistui", error.stack)
+    }
+}
+
+module.exports = router
