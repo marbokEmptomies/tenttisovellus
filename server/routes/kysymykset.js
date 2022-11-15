663f1f0ed916cb6d10e62834f833b11c07bad0f4
marbokEmptomies <markosoramaki@gmail.com>
2022-11-15 16:34:24 +0200

    transaktiolla kysymys tenttiin

--- server/routes/kysymykset.js
+++ server/routes/kysymykset.js
@@ -13,7 +13,26 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    try {
+    const client = await db.connect()
+    const { tentti_id, nimi, pisteet } = req.body
+  try {
+    await client.query('BEGIN')
+    const kysQuery = 'INSERT INTO kysymys (nimi, pisteet) VALUES($1, $2) RETURNING id'
+    const result = await client.query(kysQuery, [nimi, pisteet])
+    const kysymysTenttiin = 'INSERT INTO kysymys_tentti_liitos (tentti_id, kysymys_id) VALUES ($1, $2)'
+    const kysymysTenttiinValues = [tentti_id, result.rows[0].id]
+    await client.query(kysymysTenttiin, kysymysTenttiinValues)
+    await client.query('COMMIT')
+    res.status(200).send(`Kysymys luotu ja liitetty tenttiin, ID: ${tentti_id}`)
+  } catch (error) {
+    await client.query('ROLLBACK')
+    console.log("Virhe kysymyksen luomisessa, ", error)
+    res.status(500).send(error)
+  } finally {
+    client.release()
+  }
+})
+    /* try {
         const { tentti_id, nimi, pisteet } = req.body
         const text = 'INSERT INTO kysymys (tentti_id, nimi, pisteet) VALUES ($1, $2, $3) RETURNING id'
         const result = await db.query(text, [tentti_id, nimi, pisteet])
@@ -21,7 +40,7 @@ router.post('/', async (req, res) => {
     } catch (error) {
         console.log("Kysymyksen lisäys epäonnistui", error.stack)
     }
-})
+}) */
 
 router.put('/:id', async (req, res) => {
     try {
