e06a2923ffbafa8af215eb86b361a6a7f9078fc5
marbokEmptomies <markosoramaki@gmail.com>
2022-11-15 16:35:03 +0200

    tentin haku id:llä (subquery)

--- server/routes/tentit.js
+++ server/routes/tentit.js
@@ -1,16 +1,51 @@
+const { response } = require('express')
 const express = require('express')
+const { reset } = require('nodemon')
 const router = express.Router()
 const db = require('../database')
 
+//Hae kaikki tentit db:stä
 router.get('/', async (req, res) => {
     try {
         const result = 'SELECT * FROM tentti ORDER BY id ASC'
         const { rows } = await db.query(result)
-        res.send(rows)
+        res.status(200).send(rows)
     } catch (error) {
+        res.status(500).send(error)
         console.log("Tenttidatan hakuongelma: ", error)
     }
 })
+
+//Hae ID:llä tentti db:stä sisältäen siihen kuuluvat kysymykset ja vastausvaihtoehdot
+router.get('/:id', async (req, res) => {
+    try {
+      const haeTentti = 'SELECT * FROM tentti WHERE id = ($1)';
+      const { rows: tenttiData } = await db.query(haeTentti, [
+        req.params.id,
+      ]);
+  
+      const haeKysymys =
+        'SELECT * FROM kysymys WHERE id IN (SELECT kysymys_id FROM kysymys_tentti_liitos WHERE tentti_id = ($1))';
+      const { rows: kysymysData } = await db.query(haeKysymys, [
+        req.params.id,
+      ]);
+  
+      const haeVastaus =
+        'SELECT * FROM vastausvaihtoehto WHERE kysymys_id IN (SELECT kysymys_id FROM kysymys_tentti_liitos WHERE kysymys_id = (kysymys_id))';
+      const { rows: vastausData } = await db.query(haeVastaus);
+  
+      const tenttiObj = {
+        tentti: { ...tenttiData[0] },
+        kysymykset: [...kysymysData],
+        vastaukset: [...vastausData],
+      };
+  
+      res.status(200).send(tenttiObj);
+    } catch (error) {
+        
+        console.error("Virhe tentin haussa", error);
+    }
+  });
 
 router.post('/', async (req, res) => {
     try {
@@ -18,9 +53,10 @@ router.post('/', async (req, res) => {
         const text = 'INSERT INTO tentti (nimi) VALUES ($1)'
         const values = [nimi]
         db.query(text, values)
-        res.send("pelittää")
+        res.status(200).send("Uusi tentti lisätty.")
     } catch (error) {
-        console.log("Ei pelitä", error)
+        res.status(500).send(error)
+        console.log("Tentin lisäämisessä virhe.", error)
     }
 })
 
@@ -31,8 +67,9 @@ router.put('/:id', async (req, res) => {
         const text = 'UPDATE tentti SET nimi = ($1) WHERE id = ($2)'
         const values = [nimi, id]
         await db.query(text, values)
-        res.send(`Tentti ID:llä ${req.params.id} muokattu.`)
+        res.status(201).send(`Tentti ID:llä ${req.params.id} muokattu.`)
     } catch (error) {
+        res.status(500).send(error)
         console.log("Tentin muokkaus ei onnistunut", error.stack)
     }
 })
@@ -42,8 +79,9 @@ router.delete('/:id', async (req, res) => {
         const text = 'DELETE FROM tentti WHERE id=($1)'
         const values = [req.params.id]
         await db.query(text, values)
-        send.res(`Tentti ID:llä ${req.params.id} poistettu.`)
+        send.status(204).res(`Tentti ID:llä ${req.params.id} poistettu.`)
     } catch (error) {
+        res.status(500).send(error)
         console.log("Tentin poistaminen epäonnistui", error.stack)
     }
 })
