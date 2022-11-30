f2cddca3936348c618d9a8ff865f75c39c1ad793
marbokEmptomies <markosoramaki@gmail.com>
2022-11-30 11:15:35 +0200

    kohti seikkailua

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,40 +1,40 @@
-import axios from "axios"
-import { useEffect } from "react"
-import Kysymykset from "./Kysymykset"
+import axios from "axios";
+import { useEffect } from "react";
+import Kysymykset from "./Kysymykset";
 
 const MainContent = (props) => {
-
-    useEffect(() => {
-    const haeTenttiById = async(id) => {
-        try {
-            const result = await axios.get(`https://localhost:4000/tentit/${id}`)
-            console.log("MainContent get", result.data)
-            props.dispatch({
-              type: 'HAE_TENTTI',
-              payload: result.data
-            })
-        }catch(error) {
-            console.log("Virhe tentin haussa.", error)
-        }
+  useEffect(() => {
+    const haeTenttiById = async (id) => {
+      try {
+        const result = await axios.get(`https://localhost:4000/tentit/${id}`);
+        console.log("MainContent get", result.data);
+        props.dispatch({
+          type: "HAE_TENTTI",
+          payload: result.data,
+        });
+      } catch (error) {
+        console.log("Virhe tentin haussa.", error);
       }
-      if(props.data.valittuTentti){
-        haeTenttiById(props.data.valittuTentti);}
-    }, [props.data.valittuTentti])
+    };
+    if (props.data.valittuTentti) {
+      haeTenttiById(props.data.valittuTentti);
+    }
+  }, [props.data.valittuTentti]);
 
-    console.log(props.data.haettuTentti)
+  const kysymykset = props.data.haettuTentti?.kysymykset;
 
-    const kysymykset = props.data.haettuTentti?.kysymykset.map(item => <div>{item.nimi}</div>)
-    const vastaukset = props.data.haettuTentti?.vastaukset.map(item => <div>{item.nimi}</div>)
+  const vastaukset = props.data.haettuTentti?.vastaukset;
 
-    return (
-        <div className="main-container">
-            <h1>{props.data.haettuTentti?.tentti.nimi}</h1>
-            {/* <p>{kysymykset}</p> */}
-            <Kysymykset kysymykset={kysymykset} vastaukset={vastaukset} dispatch={props.dispatch}/>
-            {/* <p>{vastaukset}</p> */}
-        </div>
-        
-    )
-}
+  /* const vastaukset = props.data.haettuTentti?.vastaukset.map(
+    (item) => item.nimi
+  ); */
 
-export default MainContent
+  return (
+    <div className="main-container">
+      <h1>{props.data.haettuTentti?.tentti.nimi}</h1>
+      <Kysymykset kysymykset={kysymykset} vastaukset={vastaukset} />
+    </div>
+  );
+};
+
+export default MainContent;
